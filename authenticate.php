5e2d4272de57780ea1bccfc93906a99c42330141
JackCurrall <61318789+JackCurrall@users.noreply.github.com>
2020-03-26 17:46:58 +0000

    Update authenticate.php
    added comments

--- authenticate.php
+++ authenticate.php
@@ -1,18 +1,19 @@
 <?php
-include 'connection.php';
-$conn = OpenConn();
+include 'connection.php'; // this links to file including functions for Starting/Closing a connection with database
+$conn = OpenConn(); // assigning our connection to the db to a variable
 
-  $username = $_POST['uname'];
+  $username = $_POST['uname']; //retrieving values posted from form
   $password = $_POST['psw'];
-  $sql = "SELECT * FROM student WHERE student_id= '$username' AND student_password='$password'";
-  $result = mysqli_query($conn, $sql);
-  $numRows = mysqli_num_rows($result);
+  $sql = "SELECT * FROM student WHERE student_id= '$username' AND student_password='$password'"; // sql to query database for matching login info
+  $result = mysqli_query($conn, $sql); //running the qeury on the database
+  $numRows = mysqli_num_rows($result); //checking the amount of matching rows
 
-  if ($numRows == 1){
-    header('Location: homePage.html') ;
+  if ($numRows == 1){ // if there is 1 matching row then user is logged in
+    header('Location: homePage.html') ; //sends user to homepage
   }
-  else{
-    header('Location: index.html');
+  else{ //else user stays on the login page so that they can try again
+    header('Location: loginPage.html'); //sends user to login page
   }
 
 ?>
+
